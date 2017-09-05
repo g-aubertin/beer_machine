b58c8f29de0ae6f2cd2348e1b00e0c70086a16f3
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2017-09-05 16:24:49 +0200

    beer_machine.py: removing 433mhz code for now
    Signed-off-by: Guillaume Aubertin <guillaume.aubertin@gmail.com>

--- beer_machine.py
+++ beer_machine.py
@@ -4,8 +4,6 @@ import sys, os, time, signal, socket
 import sqlite3, datetime
 import webui, threading
 
-PLUG_CODE_ON = 0
-PLUG_CODE_OFF = 0
 W1_PATH = ""
 
 def socket_init():
@@ -41,15 +39,6 @@ def get_config(path):
         if (line[0] == "W1_PATH"):
             print "W1_PATH :", line[1]
             W1_PATH = line[1]
-        if (line[0] == "PLUG_CODE_ON"):
-            print "PLUG_CODE_ON :", line[1]
-            PLUG_CODE_ON = int(line[1])
-        if (line[0] == "PLUG_CODE_OFF"):
-            print "PLUG_CODE_OFF :", line[1]
-            PLUG_CODE_OFF = int(line[1])
-        if (line[0] == "STANDALONE_TEMP"):
-            print "STANDALONE_TEMP :", line[1]
-            STANDALONE_TEMP = int(line[1])
     fd_config.close()
 
 def read_temperature():
@@ -64,23 +53,6 @@ def read_temperature():
     print "current temperature :", temp_flt
     return temp_flt
 
-def plug_command(value):
-
-    cmd = "/opt/beer_machine/433_send %d %d" % (value, 24)
-    os.system(cmd)
-    if value == PLUG_CODE_ON:
-        return 1
-    else:
-        return 0
-
-#def dump_db():
-
-#    conn = sqlite3.connect('beer_machine.db')
-#    c = conn.cursor()
-#    c.execute("SELECT * FROM fermentation_temp")
-#    for data in c.fetchall():
-#        print data
-
 def worker_temp(target_temp):
 
    t = threading.currentThread()
@@ -88,12 +60,6 @@ def worker_temp(target_temp):
 
         # get temperature
         temp = read_temperature()
-
-        # adjust temperature with socket
-        if temp > target_temp + 0.5:
-             plug_command(PLUG_CODE_ON);
-        if temp < target_temp - 0.5:
-            plug_command(PLUG_CODE_OFF);
 
         time.sleep(10)
 
@@ -103,23 +69,6 @@ if __name__ == '__main__':
     if (len(sys.argv) < 2):
         print "configuration file needed"
         sys.exit(0)
-
-    # check if 433Mhz tools are compiled
-    #    if os.path.exists("/opt/beer_machine/433_send") == False:
-    #       print "433_send tool is not compiled. exiting.."
-    #       sys.exit(0)
-
-    # read configuration file and set variables
-    get_config(sys.argv[1])
-
-    # before getting started, we switch off the plug to be in a known state
-    plug_status = plug_command(PLUG_CODE_OFF)
-
-    # if standalone mode, call control loop directly (simple mode)
-    if (sys.argv[len(sys.argv)-1]) == '-s':
-        while True:
-            control_temp(STANDALONE_TEMP)
-            time.sleep(30)
 
     # database init
     conn = sqlite3.connect('beer_machine.db')
