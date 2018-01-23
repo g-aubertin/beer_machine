f285918d0bcd4425463b7c5f06b41a4c8d6e5470
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2018-01-23 16:30:54 +0100

    database rework initial draft
    Signed-off-by: Guillaume Aubertin <guillaume.aubertin@gmail.com>

---
+++ beer_db.py
@@ -0,0 +1,68 @@
+import sqlite3
+import sys
+import time
+
+class batch_status:
+    READY = "ready"
+    RUNNING = "running"
+    STOPPED = "stopped"
+    ENDED = "ended"
+
+class beer_db:
+    """ sqlite3 database wrapper to store fermentation data
+
+    Attributes:
+        file: path to database
+    """
+
+    def __init__(self, filename):
+
+        self.file = filename
+        # todo : what happens if file does not exist ?
+        try:
+            conn = sqlite3.connect(self.file)
+            c = conn.cursor()
+        except sqlite3.Error as e:
+                print ("error connecting to database, exiting...")
+                sys.exit()
+        # if this is an empty DB, create the batch list
+        c.execute('''CREATE TABLE IF NOT EXISTS batch_list
+                 (date TEXT, name TEXT, temperature float, duration INT, status TEXT)''')
+        conn.commit()
+        conn.close()
+
+    def new_batch(self, name, temperature, duration):
+
+        conn = sqlite3.connect(self.file)
+        c = conn.cursor()
+        # create new table for temperature data
+        c.execute("CREATE TABLE IF NOT EXISTS %s (date TEXT, temperature float)" % command[1])
+        # add entry in table_list : creation date - name - temperature - duration -status
+        c.execute("INSERT INTO batch_list VALUES (?, ?, ?, ?, ?)",
+            (time.ctime(), str(name), int(temperature), int(duration), batch_status.READY))
+        conn.commit()
+        conn.close()
+
+    def get_status(self):
+
+        conn = sqlite3.connect(self.file)
+        c = conn.cursor()
+        c.execute("SELECT name FROM batch_list WHERE status=?", (batch_status.RUNNING,))
+        # return a list of running batches (but there shouldn't be more than one)
+        return c.fetchall()
+
+    def change_status(self, batch_name, status):
+
+        conn = sqlite3.connect(self.file)
+        c = conn.cursor()
+        c.execute("UPDATE batch_list SET status = ? WHERE name = ?"(status, batch_name))
+        conn.commit()
+        conn.close()
+
+    def add_temperature(self, batch_name, temp):
+
+        conn = sqlite3.connect(self.file)
+        c = conn.cursor()
+        c.execute("INSERT INTO %s VALUES (?, ?)"% batch_name, (time.ctime(), temp))
+        conn.commit()
+        conn.close()
--- beer_machine.py
+++ beer_machine.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
 import sys, os, time, signal, socket
-import sqlite3, datetime
+import datetime
 import webui, threading
 from random import randint
+import beer_db
 
 W1_PATH = ""
 test_mode = 0
@@ -14,52 +14,28 @@ class daemon_status:
     RUNNING = "running"
     SHUTDOWN = "shutdown"
 
-# per batch status
-class batch_status:
-    READY = "ready"
-    RUNNING = "running"
-    STOPPED = "stopped"
-    ENDED = "ended"
-
 state = daemon_status.STOPPED
 
+# initialize UDS socket
 def socket_init():
 
     server_address = './beer_socket'
-
     # Make sure the socket does not already exist
     try:
         os.unlink(server_address)
     except OSError:
         if os.path.exists(server_address):
             raise
-
     # Create a UDS socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
     # Bind the socket to the port
     print 'UDS socket binding up on %s' % server_address
     sock.bind(server_address)
-
     # Listen for incoming connections
     sock.listen(1)
     return sock
 
-def database_init():
-
-        conn = sqlite3.connect('beer_machine.db')
-        c = conn.cursor()
-
-        # for new database
-        c.execute('''CREATE TABLE IF NOT EXISTS batch_list
-                 (date TEXT, name TEXT, temperature float, duration INT, status TEXT)''')
-
-        # check if there is a running batch in the database and set daemon_state accordingly
-        c.execute("SELECT name FROM batch_list WHERE status=?", (batch_status.RUNNING,))
-        return c.fetchall()
-
-        conn.close()
-
+# read configuration file
 def get_config(path):
 
     global W1_PATH, PLUG_CODE_ON, PLUG_CODE_OFF, STANDALONE_TEMP
@@ -72,9 +48,9 @@ def get_config(path):
             W1_PATH = line[1]
     fd_config.close()
 
+# read file from 1-wire sysfs and parse result
 def read_temperature():
 
-    # read file from 1-wire sysfs and parse result
     fd = open(W1_PATH, "r")
     temp_str = fd.read()
     fd.close()
@@ -84,14 +60,13 @@ def read_temperature():
     print "current temperature :", temp_flt
     return temp_flt
 
-def worker_temp(table):
-
-    t = threading.currentThread()
+# infinite loop getting temp from thermometer and storing it in the DB
+def worker_thread(batch_name):
 
     print "worker thread started"
+    t = threading.currentThread()
 
     while (True):
-
         if (t.stop_signal == True):
             break
         else:
@@ -100,14 +75,9 @@ def worker_temp(table):
                 temp = read_temperature()
             else:
                 temp = randint(20,25)
-
             # save temp in db
-            conn = sqlite3.connect('beer_machine.db')
-            c = conn.cursor()
-            print "adding value", temp, "to db"
-            c.execute("INSERT INTO %s VALUES (?, ?)"% table, (time.ctime(), temp))
-            conn.commit()
-            conn.close()
+            db.add_temperature(batch_name, temp)
+            print str(temp) + " added to " + batch_name
 
         time.sleep(10)
 
@@ -121,48 +91,29 @@ if __name__ == '__main__':
         print "test mode enabled"
         test_mode = 1
 
+    # initialize db
+    db = beer_db.beer_db('beer_machine.db')
+
     # create and bind socket
     uds_sock = socket_init()
 
-    # open DB and get current state
-    batch = database_init()
-
-    # if there is a running batch in DB, start the worker thread
-    if batch :
-        if len(batch) > 1:
-            print "there is more than 1 running batch in the database, you need to fix it. exiting..."
-            sys.exit(0)
-        else:
-            print "there is one on-going batch:", str(batch[0]) + ". setting daemon in RUNNING state"
-
-            t = threading.Thread(target=worker_temp, args=batch[0])
-
-            # that's the way to convert list item to string...
-            batch = ', '.join(batch[0])
-
-            t.stop_signal = False
-            t.start()
-            state = daemon_status.RUNNING
-    else:
-        batch = None
-
-    # command list :
-    # create(name, fermentation temperature, fermentation time) : create new batch
+    ############################################################################
+    # available RPC calls:
+    # create(name, fermentation temperature, fermentation time) : new batch
     # start(name): start batch
     # stop(name): stop batch
     # delete(name) : remove batch from db
+    ############################################################################
 
     while True:
-
-        # Wait for a connection
+        # Wait for a socket connection
         print 'waiting for a connection'
         connection, client_address = uds_sock.accept()
-
         print "inbound connection successful"
 
         while True:
-
             try:
+                # wait for inputs on the socket
                 data = connection.recv(256)
             except KeyboardInterrupt:
                 print "SIGINT received while waiting for data !"
@@ -170,48 +121,32 @@ if __name__ == '__main__':
                 t.join()
                 print "worker thread has been stopped. exiting...."
                 sys.exit(0)
-
             if not data:
                 break
+
+            # separate command and arguments
             command = data.split(" ")
 
+            # new batch
             if command[0] == "create" :
                 print "from socket: CREATE requested : ", command
+                db.new_batch(command[1], command[2], command[3])
 
-                conn = sqlite3.connect('beer_machine.db')
-                c = conn.cursor()
-
-                # if there is an on-going batch, change it to "ENDED"
-                # c.execute("SELECT * FROM batch_list WHERE status = 'on-going'")
-                # rows = c.fetchall()
-                # if rows:
-                # todo : pop-up to explain that the on-going batch will be moved to ENDED
-                # and move it to ENDED
-
-                # create new table
-                c.execute("CREATE TABLE IF NOT EXISTS %s (date TEXT, temperature float)" % command[1])
-                # add entry in table_list : creation date - name - temperature - duration -status
-                c.execute("INSERT INTO batch_list VALUES (?, ?, ?, ?, ?)",
-                    (time.ctime(), command[1], float(command[2]), int(command[3]), batch_status.READY))
-                conn.commit()
-                conn.close()
-
+            # status request
             if command[0] == "get_status" :
                 print "from socket: daemon STATUS requested"
-                if batch:
-                    status = "status "+ state + " " + batch
-                else:
-                    status = "status "+ state
-                connection.sendall(status)
+                connection.sendall(state)
 
+            # start aquisition
             if command[0] == "start" :
                 print "from socket: START requested for ", command[1]
-                t = threading.Thread(target=worker_temp, args=[command[1]])
+                t = threading.Thread(target=worker_thread, args=[command[1]])
                 t.stop_signal = False
                 t.start()
                 state = daemon_status.RUNNING
                 print "worker thread started"
 
+            # stop aquisition
             if command[0] == "stop" :
                 print "from socket: STOP requested"
                 t.stop_signal = True
